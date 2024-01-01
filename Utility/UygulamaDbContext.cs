9e209a43d8cab536bf58fba538bca676c89cd076
abdullah0dalati <133393460+abdullah0dalati@users.noreply.github.com>
2024-01-01 23:59:03 +0300

    Add files via upload

---
+++ Utility/UygulamaDbContext.cs
@@ -0,0 +1,20 @@
+﻿using HastaneOtomasyonASP.NET.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace HastaneOtomasyonASP.NET.Utility
+{//TABLOLARI BURAYA EKLEMELİSİN
+	public class UygulamaDbContext:IdentityDbContext
+	{
+		public UygulamaDbContext(DbContextOptions<UygulamaDbContext> options): base(options) { }
+
+		public DbSet<Doktor> Doktorlar { get; set; }//olusturulan tablonun adi
+		public DbSet<Hasta> Hastalar { get; set; }
+		public DbSet<Randevu> Randevular { get; set; }
+		public DbSet<Polikinlik> Polikinlikler { get; set; }
+		public DbSet<CalismaSaati> CalismaSaatleri { get; set; }
+		public DbSet<ApplicationUser> AplicationUsers { get; set; }
+
+
+
+    }
+}
